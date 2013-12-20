34c7f66fc2702e31abf234113fe6692695acee09
Ted Tomlinson <zumbino@gmail.com>
2013-12-20 00:54:50 -0800

    implement message passing to resolve path

--- content.js
+++ content.js
@@ -1,25 +1,14 @@
-var path = location.href;
-console.log("New location " + path);
 window.addEventListener("message", receiveMessage, false);
 var iframe = document.createElement("iframe");
 iframe.src = chrome.extension.getURL("iframe.html");
 iframe.className = 'pagerankerIFrame';
 document.body.insertBefore(iframe, document.body.firstChild);
 
-function rank(rank) {
-  // call to firebase and rankedk
-  // update dom to show the rank
-
-}
-
-function getAverageRank() {
-  // call to firebase to compute average rank for ${path}
-  return 5.1;
-}
-
 function receiveMessage(event) {
   console.log(event);
   if (event.data == 'hidePageRankFrame') {
     iframe.style.display = 'None';
+  } else if (event.data == 'getPageRankPath') {
+    event.source.postMessage({'returnPageRankPath': location.href }, '*');
   }
 }
--- pagerate.js
+++ pagerate.js
@@ -1,23 +1,42 @@
+var path = document.referrer;
+window.addEventListener("message", getPageRankPath, false);
 var firebase = new Firebase('https://pagerate.firebaseio.com/');
 var auth = new FirebaseSimpleLogin(firebase, function(error, user) {
 });
 $(document).ready(function() {
-  if (true) {
+  // get path info
+  top.postMessage('getPageRankPath', '*');
+  if (isPageRated()) {
     top.postMessage('hidePageRankFrame', '*');
     return;
   }
-    $('#login-btn').click(function() {
-          auth.login('twitter', {rememberMe: true});
-            });
+  $('#login-btn').click(function() {
+    auth.login('twitter', {rememberMe: true});
+  });
 });
 
-function rank(rank) {
+function rate(rank) {
   // call to firebase and rankedk
   // update dom to show the rank
-
 }
 
-function getAverageRank() {
+function getAverageRating() {
   // call to firebase to compute average rank for ${path}
   return 5.1;
+}
+
+function isPageRated() {
+  // TODO(call out to firebase)
+  return path.length > 0;
+}
+
+function getPageRankPath(event) {
+  if (event.data.hasOwnProperty('returnPageRankPath')) {
+    path = event.data['returnPageRankPath'];
+    console.log(path);
+    if (isPageRated()) {
+      top.postMessage('hidePageRankFrame', '*');
+      return;
+    }
+  }
 }
