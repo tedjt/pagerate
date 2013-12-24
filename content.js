ff1f2104bd11e4cea2c28f6503a1353db544e366
Gordon Wintrob <gwintrob@gmail.com>
2013-12-24 13:51:06 -0800

    Cleanup and animations

--- content.js
+++ content.js
@@ -1,14 +1,28 @@
-window.addEventListener("message", receiveMessage, false);
 var iframe = document.createElement("iframe");
-iframe.src = chrome.extension.getURL("iframe.html");
-iframe.className = 'pagerate-iframe';
-iframe.setAttribute("scrolling", "no");
-document.body.insertBefore(iframe, document.body.firstChild);
+var margin = document.createElement("div");
+
+if (!location.href.match(/api\.twitter\.com\/oauth\//)) {
+  window.addEventListener("message", receiveMessage, false);
+
+  iframe.src = chrome.extension.getURL("iframe.html");
+  iframe.className = "pagerate-iframe pagerate-hidden";
+  iframe.setAttribute("scrolling", "no");
+  document.body.insertBefore(iframe, document.body.firstChild);
+
+  margin.className = "pagerate-margin pagerate-hidden";
+  document.body.insertBefore(margin, document.body.firstChild);
+
+  setTimeout(function() {
+    iframe.className = "pagerate-iframe";
+    margin.className = "pagerate-margin";
+  }, 2000);
+}
 
 function receiveMessage(event) {
-  if (event.data == 'hidePageRankFrame') {
-    iframe.style.display = 'None';
-  } else if (event.data == 'getPageRankPath') {
-    event.source.postMessage({'returnPageRankPath': location.href }, '*');
+  if (event.data == 'hidePageRateIframe') {
+    iframe.className += " pagerate-hidden";
+    margin.className += " pagerate-hidden";
+  } else if (event.data == 'getPageRatePath') {
+    event.source.postMessage({returnPageRatePath: location.href}, '*');
   }
 }
--- pagerate.js
+++ pagerate.js
@@ -1,16 +1,67 @@
 var path, user, pageStats, averageRating, haveUpdatedStats;
 
+// structure is base/page/user.uid = 7.0
+// base/page/stats = {sum: 40, count: 14}
+var firebase = new Firebase('https://pagerate.firebaseio.com/');
+
 // register our handler to iframes that deal with auth,
 // and communicate with parent frame to show/hide toolbar
 window.addEventListener("message", handleMessage, false);
 
-// structure is base/page/user.uid = 7.0
-// base/page/stats = {sum: 40, count: 14}
-var firebase = new Firebase('https://pagerate.firebaseio.com/');
+function handleMessage(event) {
+  if (event.data.hasOwnProperty('returnPageRatePath')) {
+    var truePath = event.data.returnPageRatePath;
+    path = encodeURIComponent(truePath).replace(/\./g, '%2E');
+    // now that we loaded the path, kick off computation of average rating.
+    getPageStats();
+    maybeHideFrame();
+  } else if (event.data.hasOwnProperty('fireBaseAuthCompleted')) {
+    var localUser = event.data.fireBaseAuthCompleted;
+    if (!localUser) {
+      $('.login').removeClass('hidden');
+      return;
+    }
+
+    // login was succesful!
+    $('.current-value').removeClass('hidden');
+    var userId = localUser.uid;
+    var firebaseAuthToken = localUser.firebaseAuthToken;
+    //Log user in
+    firebase.auth(firebaseAuthToken, function(error, result) {
+      if(error) {
+        console.log("Login Failed!", error);
+      } else {
+        user = localUser;
+        console.log('Authenticated successfully with payload:', result.auth);
+        $('.login').addClass('hidden');
+        // maybeHideFrame();
+      }
+
+      $('.slider-input').mouseup(function(event) {
+        var currentRating = $(event.target).val();
+        rate(currentRating);
+        $('.current-value').css('color', '#2E9791');
+        if (!averageRating) {
+          averageRating = currentRating;
+        }
+        $('.average-value').text(averageRating + ' avg');
+        $('.average-value').css({
+          opacity: 0,
+          display: 'inline'
+        }).animate({opacity: 1}, 2000);
+      });
+    });
+  }
+}
+
+function hide() {
+  top.postMessage('hidePageRateIframe', '*');
+}
+
 $(document).ready(function() {
   // kick things off by getting the parent frame path.
   // we could also do this by passing it in the hash of the url.
-  top.postMessage('getPageRankPath', '*');
+  top.postMessage('getPageRatePath', '*');
   // we also need the user to login to show average rating + let them rate.
   var loginFunc = function() {
     var w = $('#authFrame')[0].contentWindow;
@@ -24,7 +75,7 @@ $(document).ready(function() {
   // popup option. That would also simplify all this iframe messaging
   // and these xss workarounds.
   // make hide button hide iframe.
-  $('#hide-btn').click(hide);
+  $('.hide-button').click(hide);
 
   $('.slider-input').change(function() {
     $('.current-value').text($(this).val());
@@ -57,7 +108,7 @@ function updateStats(rank) {
     return;
   }
   // update our aggregates.
-  var pageStatsRef = firebase.child(path).child('stats')
+  var pageStatsRef = firebase.child(path).child('stats');
   pageStatsRef.transaction(function(current_value) {
     haveUpdatedStats = true;
     var sumRank, sumCount;
@@ -65,10 +116,9 @@ function updateStats(rank) {
       sumRank = rank;
       sumCount = 1;
     } else {
-      sumRank = current_value['sum'] + rank;
-      sumCount = current_value['count'] + 1;
+      sumRank = current_value.sum + rank;
+      sumCount = current_value.count + 1;
     }
-    //TODO update dom to show the rank an new average.
     return {'sum': sumRank, 'count': sumCount};
   });
 }
@@ -76,7 +126,7 @@ function updateStats(rank) {
 function getPageStats() {
   // call to firebase to compute average rank for ${path}
   if (path) {
-    var pageStatsRef = firebase.child(path).child('stats')
+    var pageStatsRef = firebase.child(path).child('stats');
     pageStatsRef.once('value', function(snapshot) {
       var pageStats = snapshot.val();
       if (pageStats) {
@@ -92,6 +142,7 @@ function maybeHideFrame() {
     userPageRef.once('value', function(snapshot) {
       if(snapshot.val() === null) {
         // page is not rated;
+        return;
       } else {
         var rating = snapshot.val();
         // for now we hide.
@@ -102,51 +153,3 @@ function maybeHideFrame() {
   return false;
 }
 
-function hide() {
-  top.postMessage('hidePageRankFrame', '*');
-}
-
-function handleMessage(event) {
-  if (event.data.hasOwnProperty('returnPageRankPath')) {
-    var truePath = event.data['returnPageRankPath'];
-    path = encodeURIComponent(truePath).replace(/\./g, '%2E')
-    // now that we loaded the path, kick off computation of average rating.
-    getPageStats();
-    maybeHideFrame();
-  } else if (event.data.hasOwnProperty('fireBaseAuthCompleted')) {
-    var localUser = event.data['fireBaseAuthCompleted'];
-    if (!localUser) {
-      $('.login').removeClass('hidden');
-      return;
-    }
-
-    // login was succesful!
-    $('.current-value').removeClass('hidden');
-    var userId = localUser.uid;
-    var firebaseAuthToken = localUser.firebaseAuthToken;
-    //Log user in
-    firebase.auth(firebaseAuthToken, function(error, result) {
-      if(error) {
-        console.log("Login Failed!", error);
-      } else {
-        user = localUser;
-        console.log('Authenticated successfully with payload:', result.auth);
-        //maybeHideFrame();
-      }
-
-      $('.slider-input').mouseup(function(event) {
-        var currentRating = $(event.target).val();
-        rate(currentRating);
-        $('.current-value').css('color', '#2E9791');
-        if (!averageRating) {
-          averageRating = currentRating;
-        }
-        $('.average-value').text(averageRating + ' avg');
-        $('.average-value').css({
-          opacity: 0,
-          display: 'inline'
-        }).animate({opacity: 1}, 2000);
-      });
-    });
-  }
-}
